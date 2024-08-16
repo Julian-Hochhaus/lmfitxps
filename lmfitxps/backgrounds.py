7a75b05adbc14e058e3f2e36ccaab9b239690410
Julian-Hochhaus# <julian.hochhaus@tu-dortmund.de>
2024-08-16 15:58:25 +0200

    update version

--- lmfitxps/backgrounds.py
+++ lmfitxps/backgrounds.py
@@ -3,7 +3,7 @@ __author__ = "Julian Andreas Hochhaus"
 __copyright__ = "Copyright 2023"
 __credits__ = ["Julian Andreas Hochhaus"]
 __license__ = "MIT"
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __maintainer__ = "Julian Andreas Hochhaus"
 __email__ = "julian.hochhaus@tu-dortmund.de"
 
