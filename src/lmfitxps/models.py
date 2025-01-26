e11adfd4e9eda5015ec00afa96bcf0a63395d958
Julian-Hochhaus# <julian.hochhaus@tu-dortmund.de>
2025-01-26 16:42:57 +0100

    bump version 4.0.9 -> 4.1.0

--- src/lmfitxps/models.py
+++ src/lmfitxps/models.py
@@ -12,7 +12,7 @@ __author__ = "Julian Andreas Hochhaus"
 __copyright__ = "Copyright 2023"
 __credits__ = ["Julian Andreas Hochhaus"]
 __license__ = "MIT"
-__version__ = "4.0.9"
+__version__ = "4.1.0"
 __maintainer__ = "Julian Andreas Hochhaus"
 __email__ = "julian.hochhaus@tu-dortmund.de"
 
