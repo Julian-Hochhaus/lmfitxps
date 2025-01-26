4d2b4e72af032f7c0947af2c1e5c01ab373ebb7b
Julian-Hochhaus# <julian.hochhaus@tu-dortmund.de>
2025-01-26 15:25:35 +0100

    fixing errors

--- docs/conf.py
+++ docs/conf.py
@@ -6,17 +6,11 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 current_dir = os.path.dirname(__file__)
 target_dir = os.path.abspath(os.path.join(current_dir, "../src"))
 sys.path.insert(0, target_dir)
-
-
 
 # -- Project information -----------------------------------------------------
 
@@ -27,46 +21,50 @@ author = 'Julian A. Hochhaus'
 # The full version, including alpha/beta/rc tags
 release = "1.3.0"
 
-
 sys.path.append(os.path.abspath('exts'))
 numfig = True
+
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx_toolbox.collapse',
     'sphinx_copybutton',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',  # Add this line to enable intersphinx support
 ]
-# shpinx.ext.napoleon settings
+
 napoleon_google_docstring = False
-# Add any paths that contain templates here, relative to this directory.
+
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
 html_logo = "src/logos/logo.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
 }
-html_short_title = 'lmfitxps'
+html_short_title = project  # Use project variable for consistency
 html_favicon = "src/logos/icon.ico"
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# Enable search functionality (this should be enabled by default)
+html_search_language='en'  # Specify language if needed
+
+# Add after importing necessary libraries
+
+# Autodoc options
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'special-members': '__init__',
+}
+
+# Ensure search functionality is enabled
+html_search = True  # Generally true by default; add for clarity
